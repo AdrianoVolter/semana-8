064311f5db0db0d8f13151dba8ff0b733db7862a
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-23 12:47:30 -0300

    conctando api ao banco elephantesql

--- back-end-solar-energy/src/config/database.config.js
+++ back-end-solar-energy/src/config/database.config.js
@@ -3,20 +3,15 @@ config();
 const Sequelize = require('sequelize');
 
 module.exports = {
-  dialect: 'postgres', // Altere para 'postgres' para o ElephantSQL
+  dialect: 'postgres', 
   host: process.env.HOST,
   username: process.env.USERNAMEDB,
   password: process.env.PASSWORDDB,
   database: process.env.DATABASE,
-  port: process.env.PORT,
+  port: 5432, 
   define: {
     underscored: true,
     underscoredAll: true,
   },
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Use esta opção para evitar erros de certificado quando estiver usando SSL com o ElephantSQL
-    }
-  }
 };
+
