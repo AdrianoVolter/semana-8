f8a9351e200ddaad86fb521d93234f91fb5c6958
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-24 22:13:16 -0300

    ajuste chamada no axios

--- front-end-solar-energy/src/api/Api.jsx
+++ front-end-solar-energy/src/api/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "https://azul-verde-gaivota-toga.cíclico.app/api/v1",
+    baseURL: "https://solar-energy-api.cyclic.app//api/v1",
     responseType: "json"
 });
 
